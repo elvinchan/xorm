f140baae7e02ff59ecbb39e3639ecf7778982a5e
Lunny Xiao <xiaolunwen@gmail.com>
2017-03-14 22:25:10 +0800

    implement simple belongs_to tag

--- session.go
+++ session.go
@@ -138,7 +138,7 @@ func (session *Session) Alias(alias string) *Session {
 
 // NoCascade indicate that no cascade load child object
 func (session *Session) NoCascade() *Session {
-	session.Statement.UseCascade = false
+	session.Statement.cascadeMode = cascadeManuallyLoad
 	return session
 }
 
@@ -193,9 +193,16 @@ func (session *Session) Charset(charset string) *Session {
 
 // Cascade indicates if loading sub Struct
 func (session *Session) Cascade(trueOrFalse ...bool) *Session {
+	var mode = cascadeAutoLoad
 	if len(trueOrFalse) >= 1 {
-		session.Statement.UseCascade = trueOrFalse[0]
+		if trueOrFalse[0] {
+			mode = cascadeAutoLoad
+		} else {
+			mode = cascadeManuallyLoad
+		}
 	}
+
+	session.Statement.cascadeMode = mode
 	return session
 }
 
@@ -587,7 +594,7 @@ func (session *Session) row2Bean(rows *core.Rows, fields []string, fieldsCount i
 						session.Engine.logger.Error("sql.Sanner error:", err.Error())
 						hasAssigned = false
 					}
-				} else if col.SQLType.IsJson() {
+					/*} else if col.SQLType.IsJson() {
 					if rawValueType.Kind() == reflect.String {
 						hasAssigned = true
 						x := reflect.New(fieldType)
@@ -608,18 +615,19 @@ func (session *Session) row2Bean(rows *core.Rows, fields []string, fieldsCount i
 							}
 							fieldValue.Set(x.Elem())
 						}
-					}
-				} else if session.Statement.UseCascade {
-					table, err := session.Engine.autoMapType(*fieldValue)
-					if err != nil {
-						return nil, err
-					}
+					}*/
+				} else if (col.AssociateType == core.AssociateNone &&
+					session.Statement.cascadeMode == cascadeCompitable) ||
+					(col.AssociateType == core.AssociateBelongsTo &&
+						session.Statement.cascadeMode == cascadeAutoLoad) {
+					table := col.AssociateTable
 
 					hasAssigned = true
 					if len(table.PrimaryKeys) != 1 {
 						panic("unsupported non or composited primary key cascade")
 					}
 					var pk = make(core.PK, len(table.PrimaryKeys))
+					var err error
 					pk[0], err = asKind(vv, rawValueType)
 					if err != nil {
 						return nil, err
